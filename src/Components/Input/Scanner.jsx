b0e1143e2cf4eee7d93b426904f0ec003d483c7a
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2023-02-17 15:12:42 +0700

    bug

--- src/Components/Input/Scanner.jsx
+++ src/Components/Input/Scanner.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import QRScan from 'qrscan'
-import { Box, Flex } from '@chakra-ui/layout'
+import { Box, Center, Flex } from '@chakra-ui/layout'
 
 class Scanner extends Component {
   
   constructor (props) {
     super(props)
-    this.state = { value: '', watching: false }
+    this.state = { value: '', watching: true }
     this.onFind = this.onFind.bind(this)
   }
   
@@ -16,23 +16,24 @@ class Scanner extends Component {
   
   render () {
     return (
+        <Center>
+
       <Box>
         <h1>QRScan Demo</h1>
         {this.state.watching
           ? (
-            <Flex justify='center' w='50vw' overflow='hidden'>
-
-            <QRScan onFind={this.onFind} />
+            <Flex justify='center' w='90vw' h='40vh' overflow='hidden'>
+                <QRScan onFind={this.onFind} />
             </Flex>
           )
           : (
             <Box>
-              <button onClick={() => this.setState({ watching: true })}>Scan</button>
               <h4>value: {this.state.value}</h4>
             </Box>
           )
         }
       </Box>
+        </Center>
     )
   }
 }
