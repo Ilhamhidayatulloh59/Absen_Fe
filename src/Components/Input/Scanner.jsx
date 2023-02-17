36096c621e4aced545f5d5a27b3e36c147cfeaef
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2023-02-17 17:39:36 +0700

    Scann

--- src/Components/Input/Scanner.jsx
+++ src/Components/Input/Scanner.jsx
@@ -1,42 +1,53 @@
-import React, { Component } from 'react'
-import QrReader from 'react-weblineindia-qrcode-scanner'
+import { Button } from "@chakra-ui/button";
+import { Box, Text } from "@chakra-ui/layout";
+import React, { Component } from "react";
+import QrReader from "react-weblineindia-qrcode-scanner";
 
 class Test extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      result: 'No result',
-    }
+      result: "No result",
+    };
 
-    this.handleScan = this.handleScan.bind(this)
+    this.handleScan = this.handleScan.bind(this);
   }
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
+
+  handleScan(data) {
+    if (data !== null) {
+      localStorage.setItem("value", data);
+    }
   }
-  
-  handleError(err){
-    console.error(err)
+
+  handleError(err) {
+    console.error(err);
   }
-  render(){
+  render() {
     const previewStyle = {
       height: 240,
       width: 320,
-    }
+    };
 
-    return(
+    const value = localStorage.getItem("value");
+
+    return (
       <div>
-        <QrReader
-          delay={this.state.delay}
-        //   style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
+        {value ? (
+            <Box>
+             <Text>{value}</Text>
+             <Button onClick={() => localStorage.removeItem('value')}>Scan Ulang ?</Button>
+            </Box>
+        ) : (
+          <QrReader
+            delay={this.state.delay}
+            style={previewStyle}
+            onError={this.handleError}
+            onScan={this.handleScan}
           />
-        <p>{this.state.result}</p>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Test
+export default Test;
