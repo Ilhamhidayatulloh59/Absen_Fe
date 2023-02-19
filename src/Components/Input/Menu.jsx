68baceca4eff0300c72d4ca80895db060a2f885d
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2023-02-19 16:47:12 +0700

    fix

--- src/Components/Input/Menu.jsx
+++ src/Components/Input/Menu.jsx
@@ -12,7 +12,7 @@ import { useNavigate } from "react-router";
 
 const Menu = () => {
   const [isMobile] = useMediaQuery("(max-width: 481px)");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const MenuList = [
     { label: "Pulang" },
     { label: "Piket Jaros" },
@@ -23,27 +23,40 @@ const Menu = () => {
   ];
 
   const onNext = (item) => {
-    navigate(`/input/${item}`)
-  }
+    navigate(`/input/${item}`);
+  };
 
   return (
     <Center>
       {isMobile ? (
-        <Box mt="4" bgColor="gray.100" p="2" borderRadius="2xl">
+        <Box mt="4">
           <Text fontWeight="bold">Input</Text>
-          <Box w="90vw" justify="space-evenly" wrap="wrap" mt="4">
-            {MenuList.map((item) => {
-              return (
-                <Flex onClick={() => onNext(item.label)} align="center" m="3" p="2">
-                  <Circle bgColor="orange">
-                    <Icon p="2" color="white" w={10} h={10} as={IoAddOutline} />
-                  </Circle>
-                  <Text ml="4" fontWeight="bold" fontSize="sm">
-                    {item.label}
-                  </Text>
-                </Flex>
-              );
-            })}
+          <Box mt="2" boxShadow="base" p="2" borderRadius="sm">
+            <Box w="90vw" justify="space-evenly" wrap="wrap">
+              {MenuList.map((item) => {
+                return (
+                  <Flex
+                    onClick={() => onNext(item.label)}
+                    align="center"
+                    m="3"
+                    p="2"
+                  >
+                    <Circle bgColor="orange">
+                      <Icon
+                        p="2"
+                        color="white"
+                        w={10}
+                        h={10}
+                        as={IoAddOutline}
+                      />
+                    </Circle>
+                    <Text ml="4" fontWeight="bold" fontSize="sm">
+                      {item.label}
+                    </Text>
+                  </Flex>
+                );
+              })}
+            </Box>
           </Box>
         </Box>
       ) : (
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -8,8 +8,13 @@ import {
   Text,
   Circle,
 } from "@chakra-ui/react";
-import { IoReceiptOutline, IoPersonAddOutline, IoSettingsOutline, IoPersonCircleOutline } from "react-icons/io5";
-import {Link} from 'react-router-dom'
+import {
+  IoReceiptOutline,
+  IoPersonAddOutline,
+  IoSettingsOutline,
+  IoPersonCircleOutline,
+} from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   const [isMobile] = useMediaQuery("(max-width: 481px)");
@@ -18,28 +23,36 @@ const Menu = () => {
     { label: "input", icon: IoPersonAddOutline },
     { label: "report", icon: IoReceiptOutline },
     { label: "setting", icon: IoSettingsOutline },
-    { label: "profile", icon: IoPersonCircleOutline }
+    { label: "profile", icon: IoPersonCircleOutline },
   ];
 
   return (
     <Center>
       {isMobile ? (
-        <Box mt="4" bgColor="gray.100" p="2" borderRadius="2xl">
+        <Box mt="4">
           <Text fontWeight="bold">Main Feature</Text>
-          <Flex w="90vw" justify="space-evenly" wrap="wrap" mt="4">
-            {MenuList.map((item, index) => {
-              return (
-                <VStack key={index} w="20vw" mb="3" as={Link} to={`/${item.label}`} >
-                  <Circle bgColor="orange">
-                    <Icon p="2" color="white" w={10} h={10} as={item.icon} />
-                  </Circle>
-                  <Text fontWeight="bold" fontSize="sm">
-                    {item.label}
-                  </Text>
-                </VStack>
-              );
-            })}
-          </Flex>
+          <Box mt="2" p="2" boxShadow="base" borderRadius="md">
+            <Flex w="90vw" justify="space-evenly" wrap="wrap" mt="4">
+              {MenuList.map((item, index) => {
+                return (
+                  <VStack
+                    key={index}
+                    w="20vw"
+                    mb="3"
+                    as={Link}
+                    to={`/${item.label}`}
+                  >
+                    <Circle bgColor="orange">
+                      <Icon p="2" color="white" w={10} h={10} as={item.icon} />
+                    </Circle>
+                    <Text fontWeight="bold" fontSize="sm">
+                      {item.label}
+                    </Text>
+                  </VStack>
+                );
+              })}
+            </Flex>
+          </Box>
         </Box>
       ) : (
         "Mobile Only"
